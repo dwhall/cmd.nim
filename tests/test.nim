1986831a5a012239b84db525aa89bc5ba3e85c2e
!!Dean <dwhall256@gmail.com>
2025-09-01 11:15:32 -0500

    Made test.nim a unittest.  Enabled: nimble test

--- tests/test.nim
+++ tests/test.nim
@@ -1,6 +1,5 @@
-import sets
-import strutils
-import "../src/cmd.nim"
+import std/[strutils, unittest]
+import cmd
 
 proc echoCommand(ctx: var CmdPrompt, input: seq[string]): void =
   echo(strutils.join(input, " "))
@@ -8,8 +7,12 @@ proc echoCommand(ctx: var CmdPrompt, input: seq[string]): void =
 proc fooCommand(ctx: var CmdPrompt, input: seq[string]): void =
   echo "foo!"
 
-let echostr = Command(name: "echo", desc: "repeats back a string", help:"echos a string back", exeCmd: echoCommand)
-let foo = Command(name: "foo", desc: "prints 'foo'", help: "using this to test multiple commands", exeCmd: fooCommand)
-var my_commands: HashSet[cmd.Command] = [foo, echostr].toSet
-var prompt = cmd.CmdPrompt(commands: my_commands, promptString: "> ")
-prompt.run()
+let cmds = [
+  Command(name: "echo", desc: "repeats back a string", help:"echos a string back", exeCmd: echoCommand),
+  Command(name: "foo", desc: "prints 'foo'", help: "using this to test multiple commands", exeCmd: fooCommand)
+]
+
+test "CmdPrompt basic functionality":
+  var prompt = newCmdPrompt(commands=cmds, promptString="> ")
+  expect EOFError:
+    prompt.run()
